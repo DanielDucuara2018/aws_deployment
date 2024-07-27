8792700404cead4affa192fa1af0de006e7f6540
daniel.ducuara <daniel.ducuara@outscale.com>
2024-07-27 18:04:57 +0000

    Deploy report_calculation app

--- resources/init.sh
+++ resources/init.sh
@@ -7,3 +7,5 @@ sudo usermod -a -G docker ec2-user
 docker run --name docker-nginx -p 80:80 -d nginx:latest
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m) -o /usr/bin/docker-compose
 sudo chmod 755 /usr/bin/docker-compose
+git clone https://github.com/DanielDucuara2018/report_calculation.git /home/ec2-user/report_calculation
+git clone https://github.com/DanielDucuara2018/report_calculation_front.git /home/ec2-user/report_calculation_front
